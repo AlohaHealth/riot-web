2b82f7565fe559fa54a34efd977205a4e499f7d8
Adam Barry <adam@alohahealth.net>
2018-11-30 00:35:29 -0800

    Adds profile scope to Auth0 request

--- src/aloha/auth/Auth.js
+++ src/aloha/auth/Auth.js
@@ -1,17 +1,16 @@
-
-import auth0 from 'auth0-js';
+import auth0 from 'auth0-js'
 
 export default class Auth {
   auth0 = new auth0.WebAuth({
     domain: 'aloha-dev.auth0.com',
     clientID: 'Z8fDjG8dkxigmdp5v4QwTzOw__zjiPQI',
-    redirectUri: 'http://localhost:8080/#/callback',
+    redirectUri: 'https://auth0-web.dev.aloha.cloud/',
     responseType: 'token id_token',
-    scope: 'openid'
-  });
+    scope: 'openid profile',
+  })
 
   login() {
-    this.auth0.authorize();
+    this.auth0.authorize()
   }
 
   handleAuthentication() {
@@ -19,14 +18,12 @@ export default class Auth {
       if (authResult && authResult.accessToken && authResult.idToken) {
         // this.setSession(authResult);
         // history.replace('/home');
-        console.log(authResult);
+        console.log(authResult)
       } else if (err) {
         // history.replace('/home');
-        console.log(err);
-        alert(`Error: ${err.error}. Check the console for further details.`);
+        console.log(err)
+        alert(`Error: ${err.error}. Check the console for further details.`)
       }
-
-    });
+    })
   }
-
 }
